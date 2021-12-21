b71a02ab0a6667da42264d4cf5f04e6250160cc8
Anthony Galassi <28850131+bendhouseart@users.noreply.github.com>
2021-12-21 17:21:10 -0500

    Add cli (#2)
    * added very very basic json updater class and methods
    
    * added remove functionality
    
    * added cli, need to test
    
    * cleaned up temp files, added class calls to cli
    
    * added pyinstaller verified cli is pip installable

---
+++ json_maj/cli.py
@@ -0,0 +1,51 @@
+import argparse
+from json_maj.main import JsonMAJ
+import sys
+
+
+class ParseKwargs(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, dict())
+        for value in values:
+            key, value = value.split('=')
+            getattr(namespace, self.dest)[key] = value
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("json", metavar="json_file", help="Json file to be updated")
+parser.add_argument("--updatefile", "-f", required=False,
+                    help="Json file to update from, values in this file will be inserted into "
+                         "the file passed as the first argument for this cli.")
+parser.add_argument('--kwargs', '-k', nargs='*', action=ParseKwargs, default={},
+                    help="Include additional values into a json or override values extracted from "
+                         "the supplied json. e.g. including `--kwargs TimeZero='12:12:12'` would override the "
+                         "calculated TimeZero. Any number of additional arguments can be supplied after --kwargs "
+                         "e.g. `--kwargs BidsVariable1=1 BidsVariable2=2` etc etc.")
+parser.add_argument('--remove', '-r', nargs='*', required=False,
+                    help="Provide a key or several keys to be removed from a json in the form of `--remove key1 key2`. "
+                         "This argument can not be used with arguments that add information to a json.")
+
+
+def cli():
+    args = parser.parse_args()
+    if (args.remove and args.updatefile) or (args.remove and args.kwargs):
+        print("You must either remove data from the json or add it, you cannot do both at the same time")
+        sys.exit(1)
+
+    if args.updatefile:
+        json_maj = JsonMAJ(json_path=args.json,
+                           update_values=args.updatefile)
+        json_maj.update()
+
+    if args.kwargs:
+        json_maj = JsonMAJ(json_path=args.json,
+                           update_values=args.kwargs)
+        json_maj.update()
+
+    if args.remove:
+        json_maj = JsonMAJ()
+        json_maj.remove(args.remove)
+
+
+if __name__ == "__main__":
+    cli()
--- json_maj/main.py
+++ json_maj/main.py
@@ -1,23 +1,23 @@
 import json
-import pathlib
-import os
-import warnings
 
 
 def load_json_or_dict(json_or_dict):
     if isinstance(json_or_dict, dict):
         return json_or_dict
     elif isinstance(json_or_dict, str):
-        # try path actions on the passed variable
-        with open(json_or_dict, 'r') as infile:
-            loaded_json = json.load(infile)
-        return loaded_json
+        try:
+            # try path actions on the passed variable
+            with open(json_or_dict, 'r') as infile:
+                loaded_json = json.load(infile)
+            return loaded_json
+        except FileNotFoundError:
+            return {}
 
 
 class JsonMAJ:
-    def __init__(self, json_path, update_values):
+    def __init__(self, json_path, update_values={}):
         self.json_path = json_path
-        self.json_data = {}
+        self.json_data = load_json_or_dict(self.json_path)
         self.update_values = load_json_or_dict(update_values)
 
     def update(self):
@@ -29,18 +29,15 @@ class JsonMAJ:
                     pass
         except FileNotFoundError:
             pass
-            #warnings.warn(f"File {self.json_path} not found, creating new.")
 
         self.json_data.update(self.update_values)
 
         with open(self.json_path, 'w') as outfile:
             json.dump(self.json_data, outfile)
 
+    def remove(self, *keys):
+        for key in keys:
+            self.json_data.pop(key, None)
 
-
-
-
-
-
-
-        return
+        with open(self.json_path, 'w') as outfile:
+            json.dump(self.json_data, outfile)
